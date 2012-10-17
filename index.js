9802d0035640e8749b05ba1f529fe02f512d6796
Jacob Groundwater <groundwater@gmail.com>
2012-10-17 15:42:19 -0700

    Ship as Pre-Compiled Modules

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require("./build/Release/gcinfo")
+var fs = require('fs')
+
+var plat = process.platform
+var arch = process.arch
+
+var path = "./compiled/" + plat + "/" + arch
+
+if( !fs.existsSync(path) ){
+	throw new Error("Platform Not Supported");
+}
+
+var modpath = path + "/gcinfo"
+
+module.exports = require(modpath)
