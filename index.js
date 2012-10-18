fe1b8a565e08064d93854acf47fc7ba81df1a41d
Jacob Groundwater <groundwater@gmail.com>
2012-10-18 09:32:58 -0700

    Remove Error Message - Not Supported on Old Versions of Node

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var arch = process.arch
 
 var path = "./compiled/" + plat + "/" + arch
 
-if( !fs.existsSync(path) ){
-	throw new Error("Platform Not Supported");
-}
-
 var modpath = path + "/gcinfo"
 
 module.exports = require(modpath)
