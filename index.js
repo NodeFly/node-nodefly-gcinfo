dfaef3dd10486d5d8f29cc91fed15f8df11ac190
Jacob Groundwater <groundwater@gmail.com>
2012-11-08 11:40:34 -0800

    Use host_arch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 var plat = process.platform
-var arch = process.config.variables.target_arch
+var arch = process.config.variables.host_arch
 var vers = process.version
 
 var version_defaults = {
