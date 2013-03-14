40f7e2a127abaac2212c9a74036bd7be16724759
Eugene Kaydalov <eugene@nodefly.com>
2013-03-14 19:59:52 -0400

    removed comments intended for internal use only

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-// Author: Eugene Kaydalov
-// Date: 3/14/2013 3:59:53 PM
-//
-// Today we precompile nodefly-gcinfo module for 4 operating systems for 
+// Today we precompile nodefly-gcinfo module for 4 platforms: Windows, Solaris/SunOS, Linux, OSX
 // both 32 and 64 bit architectures.
 //
-// Frankly I don't know why we ever started doing this. One day we'll pull the trigger 
-// and let the users compile it themselves during 'npm install nodefly' or not have
-// this feature if they can't compile it (e.g. don't have all the necessary dev tools installed).
-//
-// lib_ver will contain something like 'v0.8.12' which indicates the exact minor version (.12) (or is it micro?)
+// lib_ver (example: 'v0.8.12') indicates the exact minor version (.12) (or is it micro?)
 // that the library was compiled under.
 //
-// If we can't find the mapping  we don't try a default compiled version like we used to
+// If we can't find the mapping we don't try a default compiled version like we used to
 
 var version_map = {
-	// using . instead of \\. for clarity
-	// what are the chances we'll have version 0.60?
 	'v0.6.*': 'v0.6.9',
 	'v0.8.*': 'v0.8.12',
 	'v0.9.*': 'v0.9.3',
