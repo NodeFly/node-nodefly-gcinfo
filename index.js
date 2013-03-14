89169c0e7795ab82082bda9b7e1c39fefd7005da
Eugene Kaydalov <eugene@nodefly.com>
2013-03-14 19:31:57 -0400

    full rebuild of jacob's code following our customer's complaint about unmaintainability of the map

--- index.js
+++ index.js
@@ -1,85 +1,46 @@
-var fs = require('fs')
+// Author: Eugene Kaydalov
+// Date: 3/14/2013 3:59:53 PM
+//
+// Today we precompile nodefly-gcinfo module for 4 operating systems for 
+// both 32 and 64 bit architectures.
+//
+// Frankly I don't know why we ever started doing this. One day we'll pull the trigger 
+// and let the users compile it themselves during 'npm install nodefly' or not have
+// this feature if they can't compile it (e.g. don't have all the necessary dev tools installed).
+//
+// lib_ver will contain something like 'v0.8.12' which indicates the exact minor version (.12) (or is it micro?)
+// that the library was compiled under.
+//
+// If we can't find the mapping  we don't try a default compiled version like we used to
 
-var plat = process.platform
-var arch = process.arch
-var vers = process.version
+var version_map = {
+	// using . instead of \\. for clarity
+	// what are the chances we'll have version 0.60?
+	'v0.6.*': 'v0.6.9',
+	'v0.8.*': 'v0.8.12',
+	'v0.9.*': 'v0.9.3',
+	'v0.10.*': 'v0.10.0'
+	}
+	, platform = process.platform
+	, key
+	, lib_ver
+	;
 
-if(plat=='solaris') plat='sunos'
+if (platform == 'solaris') platform = 'sunos';
 
-var version_defaults = {
-	
-	// Version 8
-	'v0.8.0': 'v0.8.12',
-	'v0.8.1': 'v0.8.12',
-	'v0.8.2': 'v0.8.12',
-	'v0.8.3': 'v0.8.12',
-	'v0.8.4': 'v0.8.12',
-	'v0.8.5': 'v0.8.12',
-	'v0.8.6': 'v0.8.12',
-	'v0.8.7': 'v0.8.12',
-	'v0.8.8': 'v0.8.12',
-	'v0.8.9': 'v0.8.12',
-	'v0.8.10': 'v0.8.12',
-	'v0.8.11': 'v0.8.12',
-	'v0.8.12': 'v0.8.12',
-	'v0.8.13': 'v0.8.12',
-	'v0.8.14': 'v0.8.12',
-	'v0.8.15': 'v0.8.12',
-	'v0.8.16': 'v0.8.12',
-	'v0.8.17': 'v0.8.12',
-	'v0.8.18': 'v0.8.12',
-	'v0.8.19': 'v0.8.12',
-	'v0.8.20': 'v0.8.12',
-	'v0.8.21': 'v0.8.12',
-	'v0.8.22': 'v0.8.12',
-	'v0.8.23': 'v0.8.12',
-	
-	// Version 6
-	'v0.6.0': 'v0.6.9',
-	'v0.6.1': 'v0.6.9',
-	'v0.6.2': 'v0.6.9',
-	'v0.6.3': 'v0.6.9',
-	'v0.6.4': 'v0.6.9',
-	'v0.6.5': 'v0.6.9',
-	'v0.6.6': 'v0.6.9',
-	'v0.6.7': 'v0.6.9',
-	'v0.6.8': 'v0.6.9',
-	'v0.6.9': 'v0.6.9',
-	'v0.6.10': 'v0.6.9',
-	'v0.6.11': 'v0.6.9',
-	'v0.6.12': 'v0.6.9',
-	'v0.6.13': 'v0.6.9',
-	'v0.6.14': 'v0.6.9',
-	'v0.6.15': 'v0.6.9',
-	'v0.6.16': 'v0.6.9',
-	'v0.6.17': 'v0.6.9',
-	'v0.6.18': 'v0.6.9',
-	'v0.6.19': 'v0.6.9',
-	'v0.6.20': 'v0.6.9',
-	'v0.6.21': 'v0.6.9',
-	'v0.6.22': 'v0.6.9',
-	
-	// Version 9
-	'v0.9.0': 'v0.9.3',
-	'v0.9.1': 'v0.9.3',
-	'v0.9.2': 'v0.9.3',
-	'v0.9.3': 'v0.9.3',
-
-	'v0.10.0': 'v0.10.0'
-	
+for (key in version_map) {
+	if (RegExp(key).test(process.version)) {
+		lib_ver = version_map[key];
+	}
 }
 
-if(!version_defaults[vers]) {
-	console.warn('nodefly-gcinfo: Unsupported Node Version: Trying v0.8.12 Compatability')
-	vers = 'v0.8.12'
-}
-
-var path = "./compiled/" + plat + "/" + arch + "/" + version_defaults[vers]
-
-var modpath = path + "/gcinfo"
+// if lib_ver stays undefined require will fail and we'll catch it
+// same behaviour when there's a new version of node and we haven't 
+// compiled for it yet
+var modpath = "./compiled/" + platform + "/" + process.arch + "/" + lib_ver + "/gcinfo";
 
 try {
-	module.exports = require(modpath)
+	module.exports = require(modpath);
 }
 catch (err) {
 	module.exports = { onGC: function(){} }
